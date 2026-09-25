f3fb33633c5b19d712f6bc5a09d3d7ec16f5256e
Navyasree1995 <Navyasree.b21@gmail.com>
2026-09-25 14:22:03 -0400

    Add CI workflow and automated tests

---
+++ tests/test_app.py
@@ -0,0 +1,12 @@
+import sys
+sys.path.insert(0, "app")
+
+from app import app
+
+
+def test_home():
+    client = app.test_client()
+    response = client.get("/")
+
+    assert response.status_code == 200
+    assert response.data == b"Hello from my DevOps project!"
